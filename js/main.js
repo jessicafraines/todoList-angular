788664bd1b60d0b005b6f623331a4a4b1cdfcd96
jessicafraines <jessicafraines@gmail.com>
2014-12-05 10:21:01 -0600

    priority options working

--- js/main.js
+++ js/main.js
@@ -9,29 +9,43 @@
           name: 'Clean house',
           desc: 'Vacuum and dust',
           due: 'Sunday',
-          priority: 'test'
+          priority: 'high'
         },
         {
           name: 'Do laundry',
           desc: 'Wash diapers and clothes',
           due: 'Sunday',
-          priority: 'test'
+          priority: 'high'
         },
         {
           name: 'Return presents',
           desc: 'Surf and Ski shop',
           due: 'Saturday',
-          priority: 'test'
+          priority: 'high'
         }
       ];
+      vm.priorityOptions = 
+      {
+        high: 'High',
+        medium: 'Medium',
+        low: 'Low'
+      };
+        
         vm.addTask = function(){
           vm.tasks.push(vm.newTask);
-          vm.newTask = null;
+          vm.newTask = _freshTask();
         };
         vm.deleteTask = function(task){
           var index = vm.tasks.indexOf(task);
           vm.tasks.splice(index, 1);
-        
         };
+
+        vm.newTask = _freshTask();
+
+        function _freshTask(){
+          return {
+            priority: 'medium'
+          }
+        }
     });
 }());
