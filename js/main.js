740d1fb66daf80c555791dc3257dab412f5ad4e8
jessicafraines <jessicafraines@gmail.com>
2014-12-09 14:36:50 -0600

    created todoFactory

--- js/main.js
+++ js/main.js
@@ -1,44 +1,97 @@
 ;(function(){
   'use strict';
 
-  angular.module("myToDoApp", ['ngRoute']) 
-    .config(function($routeProvider){
-      $routeProvider
-      .when('/', {
-        templateUrl: 'views/tasks.html',
-        controller: 'TodoController',
-        controllerAs: 'todo'
-      })
-      .when('/new', {
-        templateUrl: 'views/form.html',
-        controller: 'TodoController',
-        controllerAs: 'todo'
-      })
-      .when('/:id', {
-        templateUrl: 'views/task.html',
-        controller: 'TodoController',
-        controllerAs: 'todo'
-      })
-      .otherwise: ({redirectsTo: '/'});
+  angular.module("myToDoApp", ['ngRoute', 'mgcrea.ngStrap']) 
+  .config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+      templateUrl: 'views/tasks.html',
+      controller: 'TodoController',
+      controllerAs: 'todo'
     })
-    .controller('TodoController', function($http){
-      var vm = this;
-        
-        vm.addTask = function(){
-          vm.tasks.push(vm.newTask);
-          vm.newTask = _freshTask();
-        };
-        vm.deleteTask = function(task){
-          var index = vm.tasks.indexOf(task);
-          vm.tasks.splice(index, 1);
-        };
+    .when('/new', {
+      templateUrl: 'views/form.html',
+      controller: 'TodoController',
+      controllerAs: 'todo'
+    })
+    .when('/:id', {
+      templateUrl: 'views/task.html',
+      controller: 'ShowController',
+      controllerAs: 'show'
+    })
+    .when('/:id/edit', {
+      templateUrl: 'views/form.html',
+      controller: 'EditController',
+      controllerAs: 'todo'
+    })
+    .otherwise({redirectTo: '/'});
+  })
 
-        vm.newTask = _freshTask();
-        
-        function _freshTask(){
-          return {
-            priority: 'medium'
-          }
-        }
+  .controller('ShowController', function($http, $routeParams){
+    var vm = this;
+    var id = $routeParams.id;
+    $http.get('https://nss-todoapp.firebaseio.com/' + id + '.json')
+    .success(function(data){
+      vm.task = data;
+    })
+    .error(function(err){
+      alert('Nope');
     });
+  })//closes show controller
+
+  .controller('TodoController', function($http, $location){
+    var vm = this;
+    $http.get('https://nss-todoapp.firebaseio.com/.json')
+    .success(function(data){
+      vm.tasks = data;
+    })
+    .error(function(err){
+      alert('Not Working Bozo');
+    });
+
+    vm.addTask = function(){
+      $http.post('https://nss-todoapp.firebaseio.com/.json', vm.newTask)
+      .success(function(data){
+        vm.tasks[data.name] = vm.newTask;
+        vm.newTask = "";
+        $location.path('/');
+      })
+      .error(function(err){
+        alert('Wrong Sucka');
+      });
+    }
+    vm.deleteTask = function(id){
+      var url = 'https://nss-todoapp.firebaseio.com/' + id + '.json';
+      $http.delete(url)
+        .success(function(){
+          delete vm.tasks[id]
+        })
+      .error(function(err){
+        alert('You may never get rid of me');
+      });
+    };
+  })//closes todo controller
+
+  .controller('EditController', function($http, $routeParams, $location){
+    var vm = this;
+    var id = $routeParams.id;
+    var url = 'https://nss-todoapp.firebaseio.com/' + id + '.json'
+    $http.get(url)
+    .success(function(data){
+      vm.newTask = data;
+    })
+    .error(function(err){
+      alert('Try again Punk');
+    })
+
+    vm.addTask = function(){
+      $http.put(url, vm.newTask)
+      .success(function(data){
+        $location.path('/');
+      })
+      .error(function(err){
+        alert('WTF?');
+      });
+    }
+  })//closes edit controller
 }());
